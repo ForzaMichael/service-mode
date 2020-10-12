58dbf2271c249b499f817cb6d560d0fc88d826dd
LiQi <1372144934@qq.com>
2020-10-12 23:51:44 +0800

    tsx slot attrs

--- src/views/home/Child.tsx
+++ src/views/home/Child.tsx
@@ -1,12 +1,14 @@
 import { defineComponent, inject } from "vue";
-import  { handleDialogSymbol } from '../../../service/useDialog'
+import  { handleDialogSymbol } from '../../service/useDialog'
 export default defineComponent({
-  setup() {
+  setup(props,{slots,attrs}) {
+    console.log(attrs);
     const handleDialogService = inject(handleDialogSymbol)
     const { triggerDialog } = handleDialogService
+    console.log(slots.default());
     return () => (
       <div>
-        <el-button {...triggerDialog}>打开弹框</el-button>
+        <el-button {...triggerDialog}>{attrs['v-slots'].footer()}</el-button>
       </div>
     )
   }
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,23 +1,27 @@
 import { defineComponent, provide,withModifiers } from 'vue'
 import { handleDialogSymbol, useHandleDialog } from '../../service/useDialog'
-import Child from './components/Child'
+import Child from './Child'
 export default defineComponent({
 
   setup() {
     const handleDialogService = useHandleDialog()
-    provide(handleDialogSymbol, handleDialogService)
+    provide(handleDialogSymbol,handleDialogService)
     const { triggerDialog, dialogProps, dialogOk } = handleDialogService
     const slots={
-      footer:()=><el-button type="primary" onClick={withModifiers(dialogOk,['self'])}>确 定</el-button>
+      footer:()=><el-button type="primary" onClick={withModifiers(dialogOk,['self'])}>确dasdw 定</el-button>
     }
+    const props={props: {
+      title: '测试用的弹框',
+      width: '50%',
+    }}
     return () => (
       <div>
            <el-dialog {...dialogProps.value}>
-          <span v-slots={slots} class="dialog-footer">
+          <span  class="dialog-footer">
           </span>
         </el-dialog>
         <el-button {...triggerDialog}>弹框按钮</el-button>
-        <Child />
+        <Child v-slots={slots}>dsad</Child>
       </div>
     )
   }
